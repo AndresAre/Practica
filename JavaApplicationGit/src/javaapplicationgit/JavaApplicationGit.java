55aa5f7ed12235867671b113db48955ac26a9ccd
Alejandro <Alejandro@Alejandro>
2020-05-28 18:22:04 -0500

    Trabajo en equipo repositorio, le agrege salidas de pantalla con los nombres de los integrantes

--- JavaApplicationGit/src/javaapplicationgit/JavaApplicationGit.java
+++ JavaApplicationGit/src/javaapplicationgit/JavaApplicationGit.java
@@ -6,8 +6,10 @@
 package javaapplicationgit;
 
 /**
- *
- * @author Asus
+ *Javaaplicationgit
+ * versión 1.0
+ * Fecha 28 de mayo del 2020    
+ * Autores : Andres Arenas Y Fabio Sastoque
  */
 public class JavaApplicationGit {
 
@@ -15,9 +17,13 @@ public class JavaApplicationGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Este es mi primer repositorio \n"); // aqui aplicamos una salida por pantalla con un mensaje de texto
+        
         System.out.println("Mi nombre es Alejandro Sasroque Rincon");
+        System.out.println("Mi compañero es Andres Arenas");
         
         // TODO code application logic here
+        
     }
     
 }
